c6b1ff4fad7653fd2d941f38e3d7b70613a86692
Hadi Jeddizahed <h.jeddizahed@tgbs.co>
2019-11-02 10:03:11 +0330

    user service first commit!

---
+++ monitoring/user-service/src/main/java/com/github/userservice/api/UserServiceController.java
@@ -0,0 +1,29 @@
+package com.github.userservice.api;
+
+import com.github.userservice.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController("/api/v1")
+public class UserServiceController {
+
+    private static final Map<Integer, User> userData = new HashMap<Integer, User>() {
+
+        private static final long serialVersionUID = -3970206781360313502L;
+
+        {
+            put(111, new User(111, "User1"));
+            put(222, new User(222, "User2"));
+        }
+    };
+
+    @GetMapping("users")
+    public User getEmployeeDetails(@PathVariable int userId) {
+
+        return userData.getOrDefault(userId, new User(0, "N/A"));
+    }
+}
