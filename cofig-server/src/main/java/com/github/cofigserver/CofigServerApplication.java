324ebe11ce485cf50be2a954712465d220e3d042
Hadi Jeddizahed <h.jeddizahed@tgbs.co>
2019-11-03 16:16:59 +0330

    config-server first commit!

---
+++ cofig-server/src/main/java/com/github/cofigserver/CofigServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.cofigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CofigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CofigServerApplication.class, args);
+    }
+
+}
