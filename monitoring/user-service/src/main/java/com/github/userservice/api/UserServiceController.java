412fa7c682ec406db9e120dad0e8830ea5252ad0
Hadi Jeddizahed <h.jeddizahed@tgbs.co>
2019-11-03 11:21:24 +0330

    user service second commit!

--- monitoring/user-service/src/main/java/com/github/userservice/api/UserServiceController.java
+++ monitoring/user-service/src/main/java/com/github/userservice/api/UserServiceController.java
@@ -3,12 +3,14 @@ package com.github.userservice.api;
 import com.github.userservice.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController("/api/v1")
+@RestController
+@RequestMapping("/api/v1")
 public class UserServiceController {
 
     private static final Map<Integer, User> userData = new HashMap<Integer, User>() {
@@ -21,8 +23,8 @@ public class UserServiceController {
         }
     };
 
-    @GetMapping("users")
-    public User getEmployeeDetails(@PathVariable int userId) {
+    @GetMapping("users/{id}")
+    public User getUser(@PathVariable("id") int userId) {
 
         return userData.getOrDefault(userId, new User(0, "N/A"));
     }
