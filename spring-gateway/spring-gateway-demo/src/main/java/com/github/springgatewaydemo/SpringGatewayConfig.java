9f2e88463d83a18d352dcdb4f19c4c71cdb5f106
Hadi Jeddizahed <h.jeddizahed@tgbs.co>
2020-05-18 16:07:39 +0430

    initial

---
+++ spring-gateway/spring-gateway-demo/src/main/java/com/github/springgatewaydemo/SpringGatewayConfig.java
@@ -0,0 +1,26 @@
+package com.github.springgatewaydemo;
+
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringGatewayConfig {
+
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/product/**")
+                        .uri("https://localhost:8081/")
+                        .id("productService"))
+                .route(r -> r.path("/order/**")
+                        .uri("http://localhost:8082/")
+                        .id("orderService"))
+                .build();
+
+
+    }
+}
